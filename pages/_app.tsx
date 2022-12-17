d39358be5c66749126bb7f12bd4aa20294622180
Dan <dansfancymaths@gmail.com>
2022-12-17 14:08:46 +0330

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// need to include default mapbox css
+import "mapbox-gl/dist/mapbox-gl.css";
+// global styles
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
